c0b6ccdfbbca97b4cbff9779401c18dc4bd5ad97
snob <navistwon97@gmail.com>
2023-08-10 16:53:25 +0000

    ADD: HeungEol v1.0

---
+++ Model/model/main_process.py
@@ -0,0 +1,157 @@
+import os
+import argparse
+import torch
+import yaml
+import pandas as pd
+import pytorch_lightning as pl
+import wandb
+
+from tqdm.auto import tqdm
+from tag_models.models import Model
+from utils import utils, data_controller
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+from pytorch_lightning.loggers import WandbLogger
+from pytorch_lightning.callbacks import LearningRateMonitor
+from shutil import copyfile
+from peft import prepare_model_for_kbit_training, LoraConfig, get_peft_model, PeftModel, PeftConfig
+
+import warnings
+warnings.filterwarnings('ignore')
+
+if __name__ == "__main__":
+    """---Setting---"""
+    # argsparse 이용해서 실험명 가져오기
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--exp_name', type=str)
+    args = parser.parse_args()
+    # args.exp_name이 None이면 assert False라서 에러 발생 시키기
+    assert args.exp_name is not None, "실험명을 입력해주세요."
+    # config 파일 불러오기
+    with open('./config/use_config.yaml') as f:
+        CFG = yaml.load(f, Loader=yaml.FullLoader)
+    # 실험 결과 파일 생성 및 폴더명 가져오기
+    folder_name, save_path = utils.get_folder_name(CFG, args)
+    copyfile('./config/use_config.yaml',f"{save_path}/config.yaml")
+    pl.seed_everything(CFG['seed'])
+    # wandb 설정
+    wandb_logger = wandb.init(
+        name=folder_name, project="HeungEol", entity=CFG['wandb']['id'], dir=save_path)
+    wandb_logger = WandbLogger()
+    wandb_logger.experiment.config.update(CFG)
+
+    """---Train---"""
+    # 데이터 로더와 모델 가져오기
+    if 'HeungEol' not in CFG['train']['model_name']:
+        tokenizer = AutoTokenizer.from_pretrained(CFG['train']['model_name'])
+    else:
+        tokenizer = AutoTokenizer.from_pretrained(PeftConfig.from_pretrained(CFG['train']['model_name']).base_model_name_or_path)    
+    
+    tokenizer.pad_token = tokenizer.eos_token
+    
+    dataloader = data_controller.Dataloader(tokenizer, CFG)
+    
+    #Already Wrapped by PEFT
+    if 'HeungEol' in CFG['train']['model_name']:
+        bnb_config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_use_double_quant=True,
+            bnb_4bit_quant_type="nf4",
+            bnb_4bit_compute_dtype=torch.bfloat16)
+        
+        config = PeftConfig.from_pretrained(CFG['train']['model_name'])
+        
+        base_model = AutoModelForCausalLM.from_pretrained(
+            config.base_model_name_or_path,
+            quantization_config=bnb_config,
+            device_map={"":0},
+            low_cpu_mem_usage=True)
+        
+        base_model.gradient_checkpointing_enable()
+        base_model = prepare_model_for_kbit_training(base_model)
+        
+        LM = PeftModel.from_pretrained(base_model, CFG['train']['model_name'])
+        
+        for name, param in LM.named_parameters():
+            if "lora" in name or "Lora" in name:
+             param.requires_grad = True
+    
+    #QLoRA
+    elif CFG['adapt']['peft'] == 'QLoRA':
+        
+        bnb_config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_use_double_quant=True,
+            bnb_4bit_quant_type="nf4",
+            bnb_4bit_compute_dtype=torch.bfloat16)
+        
+        LM = AutoModelForCausalLM.from_pretrained(
+            pretrained_model_name_or_path=CFG['train']['model_name'],
+            quantization_config=bnb_config,
+            device_map={"":0},
+            low_cpu_mem_usage=True)
+        
+        LM.gradient_checkpointing_enable()
+        LM = prepare_model_for_kbit_training(LM)
+        
+        peft_config = LoraConfig(r=CFG['adapt']['r'], 
+            lora_alpha=CFG['adapt']['alpha'], 
+            target_modules=["query_key_value"], 
+            lora_dropout=CFG['adapt']['dropout'], 
+            bias="none", 
+            task_type="CAUSAL_LM")
+        
+        LM = get_peft_model(LM, peft_config)
+    
+    #LoRAFull
+    elif CFG['adapt']['peft'] == 'LoRAFull':
+        
+        LM = AutoModelForCausalLM.from_pretrained(
+            pretrained_model_name_or_path=CFG['train']['model_name'],
+            low_cpu_mem_usage=True).to(device=f"cuda", non_blocking=True)
+        
+    #LoRAfp16
+    elif CFG['adapt']['peft'] == 'LoRAfp16':
+        
+        LM = AutoModelForCausalLM.from_pretrained(
+            pretrained_model_name_or_path=CFG['train']['model_name'],
+            torch_dtype=torch.float16,
+            low_cpu_mem_usage=True).to(device=f"cuda", non_blocking=True)
+        
+    #Full-Finetuning
+    else:
+        
+        LM = AutoModelForCausalLM.from_pretrained(
+        pretrained_model_name_or_path=CFG['train']['model_name'],
+        low_cpu_mem_usage=True).to(device=f"cuda", non_blocking=True)
+    
+    utils.print_trainable_parameters(LM)
+    
+    LM.resize_token_embeddings(len(tokenizer))
+    model = Model(LM, tokenizer, CFG)
+    
+    lr_monitor = LearningRateMonitor(logging_interval='step')
+    callbacks = [lr_monitor]
+    
+    # check point
+    checkpoint = utils.LoRACheckpoint(monitor='val_loss',
+        save_top_k=3,
+        dirpath = f"{save_path}/checkpoints",
+        mode = 'min')
+    
+    callbacks.append(checkpoint)
+    
+    # Trainer
+    trainer = pl.Trainer(accelerator='gpu',
+                         precision="16-mixed" if CFG['train']['halfprecision'] else 32,
+                         accumulate_grad_batches=CFG['train']['gradient_accumulation'],
+                         max_epochs=CFG['train']['epoch'],
+                         default_root_dir=save_path,
+                         log_every_n_steps=1,
+                         val_check_interval=0.25,           # 1 epoch 당 valid loss 4번 체크: 학습여부 빠르게 체크
+                         logger=wandb_logger,
+                         callbacks=callbacks,
+                         enable_checkpointing=False
+                         )
+    """---fit---"""
+    model.LM.config.use_cache = False
+    trainer.fit(model=model, datamodule=dataloader)
---
+++ Model/model/utils/data_controller.py
@@ -0,0 +1,254 @@
+import re
+import pickle
+import torch
+import yaml
+import pandas as pd
+import pytorch_lightning as pl
+import re
+
+from sklearn.model_selection import train_test_split
+from torch.utils.data import Dataset, DataLoader
+from tqdm.auto import tqdm
+
+class Dataset(Dataset):
+    """
+    Dataloader에서 불러온 데이터를 Dataset으로 만들기
+    """
+
+    def __init__(self, data, train=False):
+        self.inputs = data
+        self.train = train
+
+    def __getitem__(self, idx):
+        if self.train:
+            inputs = {key: val[idx].clone().detach()
+                    for key, val in self.inputs.items()}
+                
+            return inputs
+        else:
+            return self.inputs[idx]
+        
+    def __len__(self):
+        if self.train:        
+            return len(self.inputs['input_ids'])
+        else:
+            return len(self.inputs)
+        
+class Dataloader(pl.LightningDataModule):
+    
+    """
+    원본 데이터를 불러와 전처리 후 Dataloader 만들어 Dataset에 넘겨 최종적으로 사용할 데이터셋 만들기
+    """
+
+    def __init__(self, tokenizer, CFG):
+        super(Dataloader, self).__init__()
+        self.CFG = CFG
+        self.tokenizer = tokenizer
+        
+        self.train_valid_df, self.predict_df = load_data()
+
+        self.train_dataset = None
+        self.val_dataset = None
+        
+        predict = self.preprocessing(self.predict_df)
+        self.predict_dataset = Dataset(predict)
+
+    def tokenizing(self, x, train=False):
+        
+        """ 
+        Arguments:
+        x: pd.DataFrame
+
+        Returns:
+        inputs: Dict({'input_ids', 'attention_mask', 'labels', ...}), 각 tensor(num_data, max_length)
+        """
+        
+        x['freq'] = [len(self.tokenizer(text)['input_ids']) for text in tqdm(x.gen_lyrics)]
+        x = x[x['freq'] <= 300]
+        
+        instruction = "다음 조건에 어울리는 가사를 생성하시오. 주어진 음절 수를 절대 벗어나지 말 것. 제목과 장르에 어울려야 할 것. 생성 형식은 ['가사 / 가사 / 가사 / 가사']와 같음."
+        x['instruction'] = instruction
+        
+        prompts_list = [f"### Instruction(명령어):\n{row['instruction']}\n\n### Input(입력):\n음절 수는 [{row['gen_notes']}], 제목은 [{row['제목']}], 장르는 [{row['장르']}]이다.\n\n### Response(응답): \n" for _ , row in x.iterrows()]
+            
+        if train:
+            answers_list = [f"{row['gen_lyrics']}" for _ , row in x.iterrows()]
+            
+            inputs = self.tokenizer(
+                prompts_list,
+                answers_list,
+                return_tensors='pt',
+                padding=True,
+                truncation='only_first',
+                max_length=self.CFG['train']['token_max_len'],
+                add_special_tokens=True,
+            )
+            
+            labels = [[-100] * (x.tolist().index(1)) + inputs['input_ids'][idx][x.tolist().index(1):].tolist() for idx, x in tqdm(enumerate(inputs['token_type_ids']))]
+            inputs['labels'] = torch.tensor(labels)
+            
+            return inputs
+
+        else:            
+            return prompts_list
+
+    def preprocessing(self, x, train=False):
+        DC = DataCleaning(self.CFG['select_DC'])
+        DA = DataAugmentation(self.CFG['select_DA'])
+
+        if train:
+            # x = DC.process(x)
+            # x = DA.process(x)         
+            
+            # 텍스트 데이터 토큰화
+            
+            # 1st finetuning
+            if 'HeungEol' not in self.CFG['train']['model_name']:
+                train_x, val_x = train_test_split(x,
+                                                test_size=self.CFG['train']['test_size'],
+                                                shuffle=True,
+                                                random_state=self.CFG['seed'])
+                
+            # 2nd finetuning
+            else:
+                val_x = x.sample(n=73, random_state=self.CFG['seed'])
+                train_x = x.drop(val_x.index)
+            
+            train_inputs = self.tokenizing(train_x, train=True)
+            
+            val_inputs = self.tokenizing(val_x, train=True)
+
+            return (train_inputs, val_inputs)
+        
+        else:
+            # x = DC.process(x)
+
+            # 텍스트 데이터 토큰화
+            predict_inputs = self.tokenizing(x, train=False)
+            
+            return predict_inputs
+
+    def setup(self, stage='fit'):
+        if stage == 'fit':
+            # 학습 데이터 준비
+            train, val = self.preprocessing(self.train_valid_df, train=True)
+            self.train_dataset = Dataset(train, train=True)
+            self.val_dataset = Dataset(val, train=True)
+        else:
+            # 평가 데이터 호출
+            predict = self.preprocessing(self.predict_df)
+            self.predict_dataset = Dataset(predict)
+
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset, batch_size=self.CFG['train']['batch_size'], shuffle=True)
+    
+    def val_dataloader(self):
+        return DataLoader(self.val_dataset, batch_size=self.CFG['train']['batch_size'], shuffle=True)
+
+    def predict_dataloader(self):
+        return DataLoader(self.predict_dataset, batch_size=self.CFG['train']['batch_size'], shuffle=False)
+    
+class DataCleaning():
+    """
+    config select DC에 명시된 Data Cleaning 기법을 적용시켜주는 클래스
+    """
+    def __init__(self, select_list):
+        self.select_list = select_list
+        
+    def process(self, df):
+        if self.select_list:
+            for method_name in self.select_list:
+                print('method name: '+ method_name)
+                
+                print('before:')
+                print(df.head(1))
+                
+                method = eval("self." + method_name)
+                df = method(df)
+                
+                print('after:')
+                print(df.head(1))
+
+        return df
+
+    """
+    data cleaning 코드
+    """
+    def data_cleaning_demo(self, df):
+        """ 
+        Arguments:
+        df: Cleaning을 수행하고자 하는 DataFrame
+        
+        Return:
+        df: Cleaning 작업이 완료된 DataFrame
+        """
+                
+        return df
+
+
+class DataAugmentation():
+    """
+    config select DA에 명시된 Data Augmentation 기법을 적용시켜주는 클래스
+    """
+
+    def __init__(self, select_list):
+        self.select_list = select_list
+
+    def process(self, df):
+        if self.select_list:
+            aug_df = pd.DataFrame(columns=df.columns)
+
+            for method_name in self.select_list:
+                method = eval("self." + method_name)
+                aug_df = pd.concat([aug_df, method(df)])
+
+            df = pd.concat([df, aug_df])
+
+        return df
+    
+    """
+    data augmentation 코드
+    """
+    
+    def data_augmentation_demo(self, df):
+        """
+
+        Arguments:
+        df: augmentation을 수행하고자 하는 DataFrame
+
+        Return:
+        df: augmentation된 DataFrame (exclude original)
+        """
+        
+        return df
+
+
+def load_data():
+    """
+    학습 데이터와 테스트 데이터 DataFrame 가져오기
+    """
+    with open('./config/use_config.yaml') as f:
+        CFG = yaml.load(f, Loader=yaml.FullLoader)
+        
+    # 1st finetuning
+    if 'HeungEol' not in CFG['train']['model_name']:
+        df = pd.read_csv('../Data/Dataset/ready/' + CFG['data_name']) 
+        df.dropna(inplace=True)
+        df['units'] = [len(x.split(' / ')) for x in df.gen_notes]
+        df = df[df['units']<70]
+        df = df[df['units'] != 1]
+        df['syls'] = [map(int, x.split(' / ')) for x in df.gen_notes]
+        df = df[~df['장르'].str.contains('랩')]
+        df = df[~df['장르'].str.contains('CCM')]
+        df = df[~df['장르'].str.contains('J-POP')]
+        df = df[~df['장르'].str.contains('-')]
+        train_valid_df, predict_df = train_test_split(df, shuffle = True, test_size=CFG['train']['test_size'], random_state=CFG['seed'])
+        train_valid_df = train_valid_df[:CFG['max_sample_num']]
+    
+    # 2nd finetuning
+    else:
+        df = pd.read_csv('./dataset/dataset_2nd_finetune_v1.0.csv')
+        predict_df = df.sample(n=100, random_state=CFG['seed'])
+        train_valid_df = df.drop(predict_df.index)
+        
+    return train_valid_df, predict_df
